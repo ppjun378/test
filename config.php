b5ac01622471e316a1ba8912999bc9c66e447d6d
ppjun378 <ppjun378@qq.com>
2020-04-09 12:06:54 +0800

    第二次测试分支提交到master 功能

--- config.php
+++ config.php
@@ -4,5 +4,6 @@
     echo 'update this is a test file2；';
     echo 'update 更新提示这是一个配置文件；';
     echo 'update 测试分支提交功能；';
-    echo 'update 测试分支提交到master功能；'
+    echo 'update 测试分支提交到master功能；';
+    echo 'update 第二次测试分支提交master功能；';
 ?>
