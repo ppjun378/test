5fc52e6d9c775b7ed4b4f97a2871b7cc4e32e8fe
ppjun378 <ppjun378@qq.com>
2020-04-09 11:46:24 +0800

    测试分支提交到master功能

--- config.php
+++ config.php
@@ -1,7 +1,8 @@
 <?php
     echo 'this is a test file;';
-    echo 'update this is a test file1';
-    echo 'update this is a test file2';
-    echo 'udate 跟新提示这是一个配置文件';
-    echo 'update 测试分支提交功能';
+    echo 'update this is a test file1；';
+    echo 'update this is a test file2；';
+    echo 'update 更新提示这是一个配置文件；';
+    echo 'update 测试分支提交功能；';
+    echo 'update 测试分支提交到master功能；'
 ?>
